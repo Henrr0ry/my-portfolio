59cf221555271476acf560c29147f53974f3d94e
Henrr0ry <honzazna@protonmail.com>
2023-09-28 21:45:59 +0200

    update the CSS

--- matrix.js
+++ matrix.js
@@ -1,89 +1,47 @@
-let streams;
+const matrixContainer = document.getElementById('matrix');
+const numRows = 30; // Počet řádků
+const numCols = 60; // Počet sloupců
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; // Seznam znaků
 
-function setup() {
-  createCanvas(innerWidth, innerHeight);
-  textFont('monospace', 16);
-  colorMode(HSL);
-  streams = Streams();
-  console.log("setup end");
+// Vytvořte prázdnou matici
+const matrix = new Array(numRows);
+
+// Inicializujte matici
+for (let i = 0; i < numRows; i++) {
+    matrix[i] = new Array(numCols).fill(' '); // Předvyplňte prázdnými znaky
 }
 
-function draw() {
-  // opacity set on background to add a bit of a blur effect
-  background(0, 0, 0, 0.6);
-  streams.draw();
-}
-
-function Streams() {
-  // set up the streams
-  const streams = [];
-  for (let i = 0; i < innerWidth / textSize(); i++) {
-    const x = i * textSize();
-    const y = round(random(-1000, 0));
-    const speed = round(random(3, 7));
-    const length = round(random(1, innerHeight / textSize()));
-    const stream = Stream(x, y, speed, length);
-    streams.push(stream);
-  }
-
-  // return object with access to draw function
-  return {
-    draw: function () {
-      for (let i = 0, length = streams.length; i < length; i++) {
-        streams[i].draw();
-      }
+// Funkce pro aktualizaci matice
+function updateMatrix() {
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+            // Náhodně vyberte znak ze seznamu
+            const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
+            matrix[i][j] = randomCharacter;
+        }
     }
-  }
 }
 
-function Stream(x, y, speed, length) {
-  // setup symbols in stream
-  const symbols = [];
-  for (let i = 0; i < length; i++) {
-    const changeRate = round(random(20, 40));
-    let lightness;
-    if (i === 0) {
-      lightness = (1 - (i / length)) * 100;
-    } else if (i === 1) {
-      lightness = (1 - (i / length)) * 95;
-    } else if (i === 2) {
-      lightness = (1 - (i / length)) * 90;
-    } else {
-      lightness = (1 - (i / length)) * 50;
+// Funkce pro zobrazení matice na stránce
+function displayMatrix() {
+    matrixContainer.innerHTML = '';
+    for (let i = 0; i < numRows; i++) {
+        const rowElement = document.createElement('div');
+        rowElement.classList.add('row');
+        for (let j = 0; j < numCols; j++) {
+            const cellElement = document.createElement('span');
+            cellElement.textContent = matrix[i][j];
+            rowElement.appendChild(cellElement);
+        }
+        matrixContainer.appendChild(rowElement);
     }
-    const colour = [120, 100, lightness];
-    const symbol = Symbol(x, y, speed, changeRate, colour);
-    symbols.push(symbol);
-    y -= textSize();
-  }
-
-  // return object with access to draw function
-  return {
-    draw: function () {
-      for (let i = 0, length = symbols.length; i < length; i++) {
-        symbols[i].draw();
-      }
-    }
-  };
 }
 
-function Symbol(x, y, speed, changeRate, colour) {
-  let character;
-
-  // return object with access to draw function
-  return {
-    draw: function () {
-      if (frameCount % changeRate === 0 || !character) {
-        character = char(round(random(65381, 65440)));
-      }
-      y = (y > height + textSize()) ? 0 : y + speed;
-      fill(colour);
-      text(character, x, y);
-    }
-  };
+// Aktualizujte a zobrazte matici pravidelně
+function animateMatrix() {
+    updateMatrix();
+    displayMatrix();
+    setTimeout(animateMatrix, 100); // Aktualizovat každých 100 ms
 }
 
-function windowResized() {
-  resizeCanvas(innerWidth, innerHeight);
-  streams = Streams();
-}
+animateMatrix(); // Spusťte animaci
