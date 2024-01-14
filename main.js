2a048d56682484c47ed49e122cae272d6565d36f
Henrr0ry <honzazna@protonmail.com>
2024-01-14 20:54:26 +0100

    website finished

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
 //Navbar
 window.onscroll = function () {
-    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600)
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
         document.getElementById("nav").classList.add("active");
-    else
+        document.getElementById("menu").classList.add("active");
+    } else {
         document.getElementById("nav").classList.remove("active");
+        document.getElementById("menu").classList.remove("active");
+    }
+
+    /*if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300)
+        document.getElementById("header-shadow").classList.add("active");
+    else
+        document.getElementById("header-shadow").classList.remove("active");*/
 };
+
+let navVisible = false;
+function nav_update() {
+    if (navVisible) {
+        document.getElementById("nav").classList.remove("show");
+        navVisible = false;
+    } else {
+        document.getElementById("nav").classList.add("show");
+        navVisible = true;
+    }
+}
+
+function update() {
+    if (window.innerWidth <= 1025)
+    {
+        nav.classList.add("sidebar");
+        menu.classList.add("show");
+    }
+    else
+    {
+        nav.classList.remove("sidebar");
+        menu.classList.remove("show");
+    }
+}
+
+window.onresize = function () {
+    update();
+}
+window.onload = function () {
+    update();
+}
 
 //Slider
 let slide = 0;
